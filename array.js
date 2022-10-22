b2f014180a7330f894433880d826dc0ed08df638
FahimS <fahim.cse.uap@gmail.com>
2022-10-22 19:26:52 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,5 @@
+var friendsAge = [11, 22, 34, 45, 43];
+var pfriendsName =['fahim', 'shahriar', 'setu','taifur','momin'];
+
+console.log(friendsAge);
+console.log(friendsAge.length);
---
+++ index.js
@@ -0,0 +1,9 @@
+var numbers = [34, 54, 56 , 67, 34];
+
+console.log(numbers[0]);
+
+numbers[1]=77;
+console.log(numbers[1]);
+
+var positionIndex =numbers.indexOf(67);
+console.log(positionIndex);
